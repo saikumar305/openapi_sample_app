6efe7da057ba8439029f938a61bedc766efd7f7e
saikumar305 <saikumarrachakonda305@gmail.com>
2022-12-31 02:40:42 +0530

    demo

--- client/main.js
+++ client/main.js
@@ -1,4 +1,7 @@
-const from = document.querySelector("form");
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
+
+const form = document.querySelector("form");
 
 const chatContainer = document.querySelector("#chat_container");
 
@@ -15,7 +18,7 @@ function loader(element) {
 }
 
 function typeText(element, text) {
-  index = 0;
+  let index = 0;
   let interval = setInterval(() => {
     if (index < text.length) {
       element.innerHTML += text.charAt(index);
@@ -36,24 +39,25 @@ function generateUniqueId() {
 
 function chatStripe(isAi, value, uniqueId) {
   return `
-        <div> 
-            <div>
-            <div>
-                <img
-                src = "${isAi ? "bot" : "user"}"
-                alt =  "${isAi ? "bot" : "user"}" />
-            </div>
-            <div class = "message" id=${uniqueId}> {value} </div>
+        <div class="wrapper ${isAi && "ai"}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? "bot" : "user"}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
             </div>
         </div>
-        `;
+    `;
 }
 
 const handleSubmit = async (e) => {
   e.preventDefault();
 
   const data = new FormData(form);
-  console.log(data);
+  console.log(data.get("prompt"));
 
   //user input
   chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
@@ -67,11 +71,37 @@ const handleSubmit = async (e) => {
 
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
+
+  //fetching data from api
+
+  const response = await fetch("http://localhost:5000/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = "";
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = await data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const error = await response.text;
+    console.log({ error });
+    messageDiv.innerHTML = "Something went wrong!";
+  }
 };
 
 form.addEventListener("submit", handleSubmit);
 form.addEventListener("keyup", (e) => {
-  if (e.keycode === 13) {
+  if (e.keyCode === 13) {
     handleSubmit(e);
   }
 });
